0f35d3e9b40ddec132067da00d9be3e4256b8475
c3sarrr <cesar140904@gmail.com>
2025-11-09 22:47:48 -0300

    feat: Adição de interface "RemoteConexao".

---
+++ src/main/java/remote/RemoteConexao.java
@@ -0,0 +1,8 @@
+package remote;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+public interface RemoteConexao extends Remote {
+
+    public  boolean testarConexao() throws RemoteException;
+}
