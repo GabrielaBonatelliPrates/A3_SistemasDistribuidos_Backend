636d350027447a0cae7945080457f27f52ed10f8
c3sarrr <cesar140904@gmail.com>
2025-11-11 19:27:08 -0300

    feat: Adição de implementacao "ConexaoImpl".

---
+++ src/main/java/implementacao/ConexaoImpl.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * @author Cesar Augusto
+ */
+package implementacao;
+
+import dao.Conexao;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.sql.Connection;
+import remote.RemoteConexao;
+
+public class ConexaoImpl extends UnicastRemoteObject implements RemoteConexao {
+
+    public ConexaoImpl() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public boolean testarConexao() throws RemoteException {
+        Connection connection = null;
+        try {
+            connection = Conexao.conectar();
+            return connection != null && !connection.isClosed();
+        } catch (Exception e) {
+            return false;
+        } finally {
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (Exception ignored) {
+            }
+        }
+    }
+}
